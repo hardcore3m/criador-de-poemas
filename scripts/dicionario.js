df3cac4abfc76a2209ab71db84a466fe3315cc3b
Rafael Marins <rafaelcunha@email.com>
2021-02-11 02:27:50 -0300

    Montando estrutura e testando codigos

---
+++ scripts/dicionario.js
@@ -0,0 +1,23 @@
+//Abstração
+var abstr = ['Uma linda tarde de outono', 'O doce beijo da felicidade', 'O mais sincero âmago', 'O desejo mais Pueril'];
+var senti = ['Amor','Esperança','Felicidade','Tristeza'];
+// Substantivos
+var sub1p = ['Meu coração', 'Minha alma', 'Minha Razão', 'Meu rancor'];
+var sub2p = ['Teu colo', 'Teu peito','Tua pele','Teu Corpo'];
+//adjetivos
+var adjfm=['Angélica', 'bela','única','vívida'];
+var adjne=['Radiante', 'Exuberante','Confortante','Invejável']
+
+//Verbos
+var pres2 = ['Fere','Entende','Supera', 'Sublima'];
+// Controle
+var qtd = sub1p.length;
+console.log('abstr = '+abstr.length);
+console.log('Senti = '+senti.length);
+console.log('Sub1p = '+sub1p.length);
+console.log('Sub2p = '+sub2p.length);
+console.log('adjfm = '+adjfm.length);
+console.log('adjne = '+adjne.length);
+console.log('pres2 = '+pres2.length);
+
+
