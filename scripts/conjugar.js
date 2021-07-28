a926a654920c4d712029deb69bb0f693467dc995
hardcore3m <rafaelcunha@email.com>
2021-07-28 13:40:40 -0300

    Trabalhando na conjugação de irregulares

---
+++ scripts/conjugar.js
@@ -0,0 +1,136 @@
+//Verbos Irregulares
+irrInfinitivo = ['ser', 'estar', 'haver', 'pôr', 'saber', 'poder', 'medir', 'fazer', 'vir', 'dar', 'trazer', 'dizer', 'querer', 'pedir', 'ouvir', 'caber']
+irrGerundio = ['sendo', 'estando', 'havendo', 'pondo', 'sabendo', 'podendo', 'medindo', 'fazendo', 'vindo', 'dando', 'trazendo', 'dizendo', 'querendo', 'pedindo', 'ouvindo', 'cabendo']
+irrParticipio = ['sido', 'estado', 'havido', 'posto']
+//Presente do indicativo
+irr1SPresInd = ['sou', 'estou', 'hei', 'ponho']
+irr2SPresInd = ['és', 'estás', 'hás', 'pões']
+irr3SPresInd = ['é', 'está', 'há', 'põe']
+irr1PPresInd = ['somos', 'estamos', 'havemos', 'pomos']
+irr2PPresInd = ['sois', 'estais', 'haveis', 'pondes']
+irr3PPresInd = ['são', 'estão', 'hão', 'põem']
+//pretérito imperfeito do indicativo
+irr1SPretImpInd = ['era', 'estava', 'havia', 'punha']
+irr2SPretImpInd = ['eras', 'estavas', 'havias', 'punhas']
+irr3SPretImpInd = ['era', 'estava', 'havia', 'punha']
+irr1PPretImpInd = ['éramos', 'estávamos', 'havíamos', 'púnhamos']
+irr2PPretImpInd = ['éreis', 'estáveis', 'havíeis', 'púnheis']
+irr3PPretImpInd = ['eram', 'estavam', 'haviam', 'punham']
+//Pretérito perfeito do indicativo
+irr1SPretPerfInd = ['fui', 'estive', 'houve', 'pus']
+irr2SPretPerfInd = ['foste', 'estiveste', 'houveste', 'puseste']
+irr3SPretPerfInd = ['foi', 'esteves', 'houve', 'pôs']
+irr1PPretPerfInd = ['fomos', 'estivemos', 'houvemos', 'pusemos']
+irr2PPretPerfInd = ['fostes', 'estivestes', 'houvestes', 'pusestes']
+irr3PPretPerfInd = ['foram', 'estiveram', 'houveram', 'puseram']
+//Pretérito mais que perfeito do indicativo
+irr1SPretMQPInd = ['fora', 'estivera', 'houvera', 'pusera']
+irr2SPretMQPInd = ['foras', 'estiveras', 'houveras', 'puseras']
+irr3SPretMQPInd = ['fora', 'estivera', 'houvera', 'puseras']
+irr1PPretMQPInd = ['fôramos', 'esvtivéramos', 'houvéramos', 'puséramos']
+irr2PPretMQPInd = ['fôreis', 'estivéreis', 'houvéreis', 'puséreis']
+irr3PPretMQPInd = ['foram', 'estiveram', 'houveram', 'puseram']
+//Futuro do presente do indicativo
+irr1SFutPresInd = ['serei', 'estarei', 'haverei', 'porei']
+irr2SFutPresInd = ['serás', 'estarás', 'haverás', 'porás']
+irr3SFutPresInd = ['será', 'estará', 'haverá', 'porá']
+irr1PFutPresInd = ['seremos', 'estaremos', 'haveremos', 'poremos']
+irr2PFutPresInd = ['sereis', 'estarieis', 'havereis', 'poreis']
+irr3PFutPresInd = ['serão', 'estarão', 'haverão', 'porão']
+//Futuro do Pretérito do indicativo
+irr1SFutPretInd = ['seria', 'estaria', 'haveria', 'poria']
+irr2SFutPretInd = ['serias', 'estarias', 'haverias', 'porias']
+irr3SFutPretInd = ['seria', 'estaria', 'haveria', 'poria']
+irr1PFutPretInd = ['seríamos', 'estaríamos', 'haveríamos', 'poríamos']
+irr2PFutPretInd = ['serieis', 'estaríeis', 'haveríeis', 'poríeis']
+irr3PFutPretInd = ['seriam', 'estariam', 'haveriam', 'poriam']
+//Presente do subjuntivo
+irr1SPresSub = ['seja', 'esteja', 'haja', 'ponha']
+irr2SPresSub = ['sejas', 'estejas', 'hajas', 'ponhas']
+irr3SPresSub = ['seja', 'esteja', 'haja', 'ponha']
+irr1PPresSub = ['sejamos', 'estejamos', 'hajamos', 'ponhamos']
+irr2PPresSub = ['sejais', 'estejais', 'hajais', 'ponhais']
+irr3PPresSub = ['sejam', 'estejam', 'hajam', 'ponham']
+//Pretérito Imperfeito do subjuntivo
+irr1SPretImpSub = ['fosse', 'estivesse', 'houvesse', 'pusesse']
+irr2SPretImpSub = ['fosses', 'estivesses', 'houvesses', 'pusesses']
+irr3SPretImpSub = ['fosse', 'estivesse', 'houvesse', 'pusesse']
+irr1PPretImpSub = ['fôssemos', 'estivéssemos', 'houvéssemos', 'puséssemos']
+irr2PPretImpSub = ['fosseis', 'estivésseis', 'houvésseis', 'pusésseis']
+irr3PPretImpSub = ['fossem', 'estivessem', 'houvessem', 'pusessem']
+//Futuro do subjuntivo
+irr1SFutSub = ['for', 'estiver', 'houver', 'puser']
+irr2SFutSub = ['fores', 'estiveres', 'houveres', 'puseres']
+irr3SFutSub = ['for', 'estiver', 'houver', 'puser']
+irr1PFutSub = ['formos', 'estivermos', 'houvermos', 'pusermos']
+irr2PFutSub = ['fordes', 'estiverdes', 'houverdes', 'puserdes']
+irr3PFutSub = ['forem', 'estiverem', 'houverem', 'puserem']
+//Imperativo Afirmativo
+irr2SImpAf = ['sê', 'está', 'há', 'põe']
+irr3SImpAf = ['seja', 'esteja', 'haja', 'ponha']
+irr1PImpAf = ['sejamos', 'estejamos', 'hajamos', 'ponhamos']
+irr2PImpAf = ['sede', 'estai', 'havei', 'ponde']
+irr3PImpAf = ['sejam', 'estejam', 'hajam', 'ponham']
+//Imperativo Afirmativo
+irr2SImpNeg = ['sejas', 'estejas', 'hajas', 'põe']
+irr3SImpNeg = ['seja', 'esteja', 'haja', 'ponha']
+irr1PImpNeg = ['sejamos', 'estejamos', 'hajamos', 'ponhamos']
+irr2PImpNeg = ['sejais', 'estejais', 'hajais', 'ponde']
+irr3PImpNeg = ['sejam', 'estejam', 'hajam', 'pondam']
+//Imfinitivo Pessoal
+irr1SInfPes = ['ser', 'estar', 'haver']
+irr2SInfPes = ['seres', 'estares', 'haveres']
+irr3SInfPes = ['ser', 'estar', 'haver']
+irr1PInfPes = ['sermos', 'estarmos', 'havermos']
+irr2PInfPes = ['serdes', 'estardes', 'haverdes']
+irr3PInfPes = ['serem', 'estarem', 'haverem']
+
+
+userVerbo = 'ser';
+function conjugar() {
+    let verboIndex = irrInfinitivo.indexOf(userVerbo)
+    if (irrInfinitivo.includes(userVerbo)===true) {
+        $("#conjugarTitulo").html(`Verbo: "${irrInfinitivo[verboIndex]}" - Verbo Irregular`)
+        $("#conjugarVerbo").html(`
+    <li><b>Infinitivo: </b>${irrInfinitivo[verboIndex]}</li>
+    <li><b>Gerundio: </b>${irrGerundio[verboIndex]}</li>
+    <li><b>Particípio: </b>${irrParticipio[verboIndex]}</li>
+    <h2>Presente do indicativo</h2>
+    <li><b>Eu </b>${irr1SPresInd[verboIndex]}</li>
+    <li><b>Tu </b>${irr2SPresInd[verboIndex]}</li>
+    <li><b>Ele, Ela </b>${irr3SPresInd[verboIndex]}</li>
+    <li><b>Nós </b>${irr1PPresInd[verboIndex]}</li>
+    <li><b>Vós </b>${irr2PPresInd[verboIndex]}</li>
+    <li><b>Eles, Elas </b>${irr3PPresInd[verboIndex]}</li>
+
+    <h2>Pretérito imprefeito do indicativo </h2>
+    <li><b>Eu </b>${irr1SPretImpInd[verboIndex]}</li>
+    <li><b>Tu </b>${irr2SPretImpInd[verboIndex]}</li>
+    <li><b>Ele, Ela </b>${irr3SPretImpInd[verboIndex]}</li>
+    <li><b>Nós </b>${irr1PPretImpInd[verboIndex]}</li>
+    <li><b>Vós </b>${irr2PPretImpInd[verboIndex]}</li>
+    <li><b>Eles, Elas </b>${irr3PPretImpInd[verboIndex]}</li>
+
+    <h2>Pretérito perfeito do indicativo </h2>
+    <li><b>Eu </b>${irr1SPretPerfInd[verboIndex]}</li>
+    <li><b>Tu </b>${irr2SPretPerfInd[verboIndex]}</li>
+    <li><b>Ele, Ela </b>${irr3SPretPerfInd[verboIndex]}</li>
+    <li><b>Nós </b>${irr1PPretPerfInd[verboIndex]}</li>
+    <li><b>Vós </b>${irr2PPretPerfInd[verboIndex]}</li>
+    <li><b>Eles, Elas </b>${irr3PPretPerfInd[verboIndex]}</li>
+
+    <h2>Pretérito mais que perfeito do indicativo </h2>
+    <li><b>Eu </b>${irr1SPretMQPInd[verboIndex]}</li>
+    <li><b>Tu </b>${irr2SPretMQPInd[verboIndex]}</li>
+    <li><b>Ele, Ela </b>${irr3SPretMQPInd[verboIndex]}</li>
+    <li><b>Nós </b>${irr1PPretMQPInd[verboIndex]}</li>
+    <li><b>Vós </b>${irr2PPretMQPInd[verboIndex]}</li>
+    <li><b>Eles, Elas </b>${irr3PPretMQPInd[verboIndex]}</li>
+    `)
+        
+        
+    } else {
+        console.log(`Verbo: "${userVerbo}" - Verbo Regular`);
+    }    
+}
+
